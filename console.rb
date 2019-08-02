9866f32a87a8d816d7a5a25e118e83b0ba293b94
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-02 13:24:12 +0100

    Creates instances of customer class

---
+++ console.rb
@@ -0,0 +1,32 @@
+require("pry-byebug")
+
+customer1 = Customer.new(
+  {
+    "name" => "Phoebe",
+    "funds" => 50
+  }
+)
+
+customer2 = Customer.new(
+  {
+    "name" => "Piper",
+    "funds" => 40
+  }
+)
+
+customer3 = Customer.new(
+  {
+    "name" => "Prue",
+    "funds" => 70
+  }
+)
+
+
+
+
+
+
+
+binding.pry
+
+nil
