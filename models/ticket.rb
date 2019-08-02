5cc6e785f2631aad2cadc914f451f0392917545c
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-08-02 14:28:19 +0100

    Creates ticket class and checks in pry

---
+++ models/ticket.rb
@@ -0,0 +1,18 @@
+require_relative("../db/sql_runner.rb")
+
+class Ticket
+
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
+
+  def initialize(ticket)
+    @id = ticket["id"].to_i if ticket["id"]
+    @customer_id = ticket["customer_id"].to_i
+    @film_id = ticket["film_id"].to_i
+  end
+
+
+
+
+
+end
